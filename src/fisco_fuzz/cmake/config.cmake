b9249b7fb03c07f216cd407e0ea70fa2f1ee9f2b
ConsensusFuzz <consensusFuzz@163.com>
2023-06-12 15:40:22 +0800

    add fisco source

---
+++ src/fisco_fuzz/cmake/config.cmake
@@ -0,0 +1,94 @@
+hunter_config(bcos-utilities VERSION 3.0.0-rc2-local
+    URL https://${URL_BASE}/FISCO-BCOS/bcos-utilities/archive/b94375d933cc65c348b5c219f2ed821a5f66d9b1.tar.gz
+	SHA1 2ae9c579e924ccb67f07c9626d4d748bc60c9cef
+    CMAKE_ARGS HUNTER_PACKAGE_LOG_BUILD=ON HUNTER_PACKAGE_LOG_INSTALL=ON
+)
+
+hunter_config(bcos-boostssl
+    VERSION 3.0.0-rc2-local
+    URL https://${URL_BASE}/FISCO-BCOS/bcos-boostssl/archive/047ac9c246109792d11e9d99e5f5b69966b7fb1f.tar.gz
+    SHA1 b11cc10ba47e85c3d3e7f50dc07ef81d387f737d
+    CMAKE_ARGS HUNTER_PACKAGE_LOG_BUILD=ON HUNTER_PACKAGE_LOG_INSTALL=ON URL_BASE=${URL_BASE}
+)
+
+hunter_config(rocksdb VERSION 6.20.3
+	URL  https://${URL_BASE}/facebook/rocksdb/archive/refs/tags/v6.20.3.tar.gz
+    SHA1 64e4e6031820026c051d6e2072c0197e3bce1643
+    CMAKE_ARGS WITH_TESTS=OFF WITH_GFLAGS=OFF WITH_BENCHMARK_TOOLS=OFF WITH_CORE_TOOLS=OFF
+    WITH_TOOLS=OFF PORTABLE=ON FAIL_ON_WARNINGS=OFF WITH_ZSTD=ON BUILD_SHARED_LIBS=OFF
+)
+
+hunter_config(evmc VERSION 7.3.0-c7feb13f
+	URL  https://${URL_BASE}/FISCO-BCOS/evmc/archive/c7feb13f582919242da9f4f898ed4578785c9ecc.tar.gz
+	SHA1 28ab1c74dd3340efe101418fd5faf19d34c9f7a9
+    CMAKE_ARGS URL_BASE=${URL_BASE}
+)
+
+hunter_config(intx VERSION 0.4.1 URL https://${URL_BASE}/chfast/intx/archive/v0.4.0.tar.gz
+    SHA1  8a2a0b0efa64899db972166a9b3568a6984c61bc
+	CMAKE_ARGS CMAKE_CXX_FLAGS=-std=c++17
+)
+
+hunter_config(ethash VERSION 0.7.0-4576af36 URL https://${URL_BASE}/chfast/ethash/archive/4576af36f8ebb9bee2d5f04be692f295c64a7211.tar.gz
+	SHA1 2001a265177c722b4cbe91c4160f3f582e0c9938
+	CMAKE_ARGS CMAKE_CXX_FLAGS=-std=c++17
+)
+
+hunter_config(evmone VERSION 0.4.1-b726a1e1
+	URL https://${URL_BASE}/FISCO-BCOS/evmone/archive/b726a1e1722109291ac18bc7c5fad5aac9d2e8c5.tar.gz
+	SHA1 e41fe0514a7a49a9a5e7eeb1b42cf2c3ced67f5d
+	CMAKE_ARGS CMAKE_CXX_FLAGS=-std=c++17 BUILD_SHARED_LIBS=OFF
+)
+
+hunter_config(tarscpp VERSION 3.0.3-7299ad23)
+
+hunter_config(
+    Boost VERSION "1.76.0-local"
+    URL
+    "https://osp-1257653870.cos.ap-guangzhou.myqcloud.com/FISCO-BCOS/FISCO-BCOS/deps/boost_1_76_0.tar.bz2
+    https://downloads.sourceforge.net/project/boost/boost/1.76.0/source/boost_1_76_0.tar.bz2
+    https://nchc.dl.sourceforge.net/project/boost/boost/1.76.0/boost_1_76_0.tar.bz2"
+    SHA1
+    8064156508312dde1d834fec3dca9b11006555b6
+    CMAKE_ARGS
+    CONFIG_MACRO=BOOST_UUID_RANDOM_PROVIDER_FORCE_POSIX
+)
+
+hunter_config(
+    Protobuf VERSION "3.14.0-4a09d77-p0-local"
+    URL
+    "https://osp-1257653870.cos.ap-guangzhou.myqcloud.com/FISCO-BCOS/deps/protobuf/v3.14.0-4a09d77-p0.tar.gz
+    https://github.com/cpp-pm/protobuf/archive/v3.14.0-4a09d77-p0.tar.gz"
+    SHA1
+    3553ff3bfd7d0c4c1413b1552064b3dca6fa213e
+)
+
+hunter_config(
+    Microsoft.GSL VERSION "2.0.0-p0-local"
+    URL
+    "https://osp-1257653870.cos.ap-guangzhou.myqcloud.com/FISCO-BCOS/deps/Microsoft.GSL/v2.0.0-p0.tar.gz
+    https://github.com/hunter-packages/Microsoft.GSL/archive/v2.0.0-p0.tar.gz"
+    SHA1
+    a94c9c1e41edf787a1c080b7cab8f2f4217dbc4b
+)
+
+# hunter_config(
+#     OpenSSL VERSION "tassl_1.1.1b_v1.4-63b60292"
+#     URL https://codeload.github.com/FISCO-BCOS/TASSL-1.1.1b/zip/63b602923f924b432774f6b6a2b22c708d5231c8
+#     SHA1 d4ffbdc5b29cf437f5f6711cc3d4b35f04b06965
+# )
+
+hunter_config(
+    jsoncpp VERSION "1.8.0-local"
+    URL
+    "https://osp-1257653870.cos.ap-guangzhou.myqcloud.com/FISCO-BCOS/deps/jsoncpp/1.8.0.tar.gz
+    https://github.com/open-source-parsers/jsoncpp/archive/1.8.0.tar.gz"
+    SHA1
+    40f7f34551012f68e822664a0b179e7e6cac5a97
+)
+hunter_config(tbb VERSION 2021.3.0 CMAKE_ARGS BUILD_SHARED_LIBS=OFF)
+hunter_config(ZLIB VERSION ${HUNTER_ZLIB_VERSION} CMAKE_ARGS CMAKE_POSITION_INDEPENDENT_CODE=TRUE)
+hunter_config(c-ares VERSION 1.14.0-p0 CMAKE_ARGS CMAKE_POSITION_INDEPENDENT_CODE=TRUE CARES_BUILD_TOOLS=OFF)
+hunter_config(re2 VERSION ${HUNTER_re2_VERSION} CMAKE_ARGS CMAKE_POSITION_INDEPENDENT_CODE=TRUE RE2_BUILD_TESTING=OFF)
+hunter_config(abseil VERSION ${HUNTER_abseil_VERSION} CMAKE_ARGS CMAKE_CXX_FLAGS=-std=c++11 CMAKE_POSITION_INDEPENDENT_CODE=TRUE ABSL_ENABLE_INSTALL=ON ABSL_RUN_TESTS=OFF)
+hunter_config(gRPC VERSION ${HUNTER_gRPC_VERSION} CMAKE_ARGS CMAKE_CXX_FLAGS=-std=c++17 CMAKE_POSITION_INDEPENDENT_CODE=TRUE)
